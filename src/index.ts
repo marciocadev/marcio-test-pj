26fd35b8e8680b856d2a5302fd1190b115479e2a
Marcio Almeida <marciocadev@gmail.com>
2022-07-30 11:29:55 -0300

    feat: readme

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,20 @@ import { TypeScriptProject, TypeScriptProjectOptions } from 'projen/lib/typescri
 
 export class MarcioTestPj extends TypeScriptProject {
   constructor(options: TypeScriptProjectOptions) {
+    const readme = `#${options.name}
+
+Basic project
+`;
     super({
       github: false,
+      projenrcTs: true,
+      readme: {
+        filename: 'README.md',
+        contents: readme,
+      },
       ...options,
     });
+
+
   }
 }
