bf34f2871d8b15bff43b0b7656d2439cbae08e0e
Marcio Almeida <marciocadev@gmail.com>
2022-07-30 12:24:05 -0300

    feat: sei la

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { YamlFile } from 'projen';
+import { NodePackage, NodePackageOptions } from 'projen/lib/javascript';
 import { TypeScriptProject, TypeScriptProjectOptions } from 'projen/lib/typescript';
 
 export class MarcioTestPj extends TypeScriptProject {
@@ -17,11 +18,34 @@ Basic project
       deps: [
         'serverless',
         'serverless-esbuild',
-        '#types/aws-lambda',
+        '@types/aws-lambda',
         '@aws-lambda-powertools/logger',
       ],
       ...options,
     });
+
+    this.package.removeScript('build');
+    this.package.removeScript('bump');
+    this.package.removeScript('clobber');
+    //this.package.removeScript('default');
+    this.package.removeScript('eject');
+    //this.package.removeScript('eslint');
+    this.package.removeScript('package');
+    this.package.removeScript('post-compile');
+    this.package.removeScript('post-upgrade');
+    this.package.removeScript('pre-compile');
+    this.package.removeScript('release');
+    //this.package.removeScript('test');
+    this.package.removeScript('test:update');
+    this.package.removeScript('test:watch');
+    this.package.removeScript('unbump');
+    this.package.removeScript('upgrade');
+    this.package.removeScript('watch');
+    //this.package.removeScript('projen');
+
+    this.package.setScript('deploy', 'sls deploy');
+    this.package.setScript('remove', 'sls remove');
+    this.package.setScript('package', 'sls package');
   }
 
   synth(): void {
