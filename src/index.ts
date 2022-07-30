326c7e2a17c18f14da72212d32c1582b6efe27a7
Marcio Almeida <marciocadev@gmail.com>
2022-07-30 11:49:52 -0300

    feat: yaml

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { YamlFile } from 'projen';
 import { TypeScriptProject, TypeScriptProjectOptions } from 'projen/lib/typescript';
 
 export class MarcioTestPj extends TypeScriptProject {
   constructor(options: TypeScriptProjectOptions) {
-    const readme = `#${options.name}
+    const readme = `# ${options.name}
 
 Basic project
 `;
@@ -13,9 +14,50 @@ Basic project
         filename: 'README.md',
         contents: readme,
       },
+      deps: [
+        'serverless',
+        'serverless-esbuild',
+        '#types/aws-lambda',
+        '@aws-lambda-powertools/logger',
+      ],
       ...options,
     });
+  }
 
-
+  synth(): void {
+    new YamlFile(this, 'bitbucket-pipelines.yml', {
+      obj: {
+        pipelines: {
+          branches: [
+            {
+              'feature/*': [
+                { step: '*npm-test' },
+                { step: '*npm-install' },
+                {
+                  step: {
+                    '<<': '*sls-deploy',
+                    'name': 'Dev',
+                    'deployment': 'dev',
+                  },
+                },
+              ],
+            },
+            {
+              develop: [
+                { step: '*npm-test' },
+                { step: '*npm-install' },
+                {
+                  step: {
+                    '<<': '*sls-deploy',
+                    'name': 'Dev',
+                    'deployment': 'dev',
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    });
   }
 }
